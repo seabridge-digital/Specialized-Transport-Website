5795fd2ea497de1f22f8fdefdd68ab2cd73f0cf4
seabridge-digital <cary@seabridge.digital>
2025-11-15 06:55:50 -0500

    Refactor footer component to use functional syntax

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,13 +1,21 @@
-<footer className="fixed bottom-0 left-0 w-full bg-white border-t border-gray-200 z-50">
-  <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6 flex flex-col md:flex-row justify-between items-center text-gray-700">
-    {/* Clickable Brand */}
-    <Link href="/" className="text-lg font-semibold hover:text-blue-600">
-      Specialized Transport
-    </Link>
+"use client";
 
-    {/* Phone Number */}
-    <a href="tel:4054136077" className="mt-2 md:mt-0 hover:text-blue-600">
-      (405) 413-6077
-    </a>
-  </div>
-</footer>
+import Link from "next/link";
+
+export default function Footer() {
+  return (
+    <footer className="fixed bottom-0 left-0 w-full bg-white border-t border-gray-200 z-50">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6 flex flex-col md:flex-row justify-between items-center text-gray-700">
+        {/* Clickable Brand */}
+        <Link href="/" className="text-lg font-semibold hover:text-blue-600">
+          Specialized Transport
+        </Link>
+
+        {/* Phone Number */}
+        <a href="tel:4054136077" className="mt-2 md:mt-0 hover:text-blue-600">
+          (405) 413-6077
+        </a>
+      </div>
+    </footer>
+  );
+}
