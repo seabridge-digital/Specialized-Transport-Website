41f32baa2c097d95af6319e1d284f7c4cea488e4
seabridge-digital <cary@seabridge.digital>
2025-11-15 06:57:13 -0500

    Add padding to main content area

--- app/layout.tsx
+++ app/layout.tsx
@@ -62,7 +62,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col`}
       >
         <Header />
-        <main className="flex-1">{children}</main>
+        <main className="flex-1 pb-24">{children}</main>
         <Footer />
       </body>
     </html>
